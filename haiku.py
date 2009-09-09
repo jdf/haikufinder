19c7cdd708e1de7b05b91fa5ebb323262f110182
Jonathan Feinberg <jdf@pobox.com>
2009-09-08 23:05:21 -0400

    made into a web service ha ha ha ha ha

--- haiku.py
+++ haiku.py
@@ -4,20 +4,27 @@ import sys
 import nltk
 import re
 
-text = open(sys.argv[1],'r').read()
+#text = open(sys.argv[1],'r').read()
 
 syllables = dict()
-force_valid = re.compile(r'[.?!"\']$')
-invalid_sentence = re.compile(r'[.?!;:]+\s*.')
+bad_end = re.compile(r'[.?!;:]+\s*.|\b(?:a|the|an|he|she|I|we|they|as|of|and|with|my|your)$', re.IGNORECASE)
+ok_end = re.compile(r'[.?!"\']$')
+object_in_front = re.compile(r'^(?:him|me|us|them)\b', re.IGNORECASE)
 
+parenned = re.compile(r'\(\d+\)')
 for line in file('cmudict.0.7a'):
     (word, sp, phonemes) = line.partition(' ')
     if '(' in word:
-        continue
+        word = parenned.sub('',word)
     count = len([x for x in list(phonemes) if x >= '0' and x <= '9'])
+    if syllables.has_key(word):
+        count = min(count, syllables[word])
     syllables[word] = count
 
 class Nope(Exception):
+    pass
+
+class TooShort(Exception):
     pass
 
 class Syllablizer:
@@ -42,11 +49,13 @@ class Syllablizer:
         except KeyError:
             raise Nope
         except IndexError:
-            raise Nope
+            raise TooShort
         if syllable_count > n:
             raise Nope
         line = ' '.join(self.words[si:self.index])
-        if invalid_sentence.search(line) and not force_valid.search(line):
+        if object_in_front.search(line):
+            raise Nope
+        if bad_end.search(line) and not ok_end.search(line):
             raise Nope
         self.lines.append(line)
     
@@ -65,13 +74,63 @@ class Syllablizer:
         stream.write("    %s\n"%self.lines[1])
         stream.write("%s\n\n"%self.lines[2])
         
-
-sentence_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-
-for line in sentence_tokenizer.tokenize(text):
+def syllablize(line, out):
     syl = Syllablizer(line)
     try:
         syl.find_haiku()
-        syl.dump(sys.stdout)
+        syl.dump(out)
     except Nope:
-        continue
+        pass
+
+sentence_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+
+import cherrypy
+import cStringIO
+
+class HaikuSite:
+
+    @cherrypy.expose
+    def index(self):
+        # Ask for the user's name.
+        return '''
+            <form action="findhaikus" method="POST">
+            Enter some text here:<br>
+            <input type="submit" /><br>
+            <textarea rows="40" cols="80" name="text"></textarea>
+            </form>'''
+    
+    @cherrypy.expose
+    def findhaikus(self, text = None):
+        if text:
+            cherrypy.response.headers['Content-Type'] = 'text/plain'
+            out = cStringIO.StringIO()
+            lines = sentence_tokenizer.tokenize(text)
+            line_index = 0
+            line_count = len(lines)
+            while line_index < line_count:
+                line = lines[line_index]
+                offset = 0
+                while True:
+                    try:
+                        syllablize(line, out)
+                    except TooShort:
+                        offset += 1
+                        if line_index + offset >= line_count:
+                            break
+                        line = "%s %s" % (line, lines[line_index + offset])
+                    break
+                line_index += 1
+            haikus = out.getvalue()
+            out.close()
+            return haikus
+        else:
+            cherrypy.response.headers['Location'] = '/'
+            cherrypy.response.status = 302
+
+cherrypy.tree.mount(HaikuSite())
+
+if __name__ == '__main__':
+    import os.path
+    thisdir = os.path.dirname(__file__)
+    cherrypy.quickstart(config=os.path.join(thisdir, 'haiku.conf'))
