185951ca5546bfea0e410b7b77244967fac95ee3
Jonathan Feinberg <jdf@pobox.com>
2009-09-13 17:35:58 -0400

    moved webapp to its own project

---
+++ haikufinder/cmudict/PickleCMUDict.py
@@ -0,0 +1,43 @@
+#!/bin/env python
+
+# Read the CMU pronounciation dictionary, count syllables (throwing away
+# phoneme and stress information), and pickle the result.
+#
+#
+# Copyright (c) 2009, Jonathan Feinberg <jdf@pobox.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#   1. Redistributions of source code must retain the above copyright notice, 
+#      this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright notice,
+#      this list of conditions and the following disclaimer in the documentation
+#      and/or other materials provided with the distribution.
+#   3. The name of the author may not be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED 
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR 
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+import cPickle as pickle
+from nltk.corpus import cmudict
+import re
+syllables = dict()
+numeral = re.compile(r'\d')
+parenned = re.compile(r'\(\d+\)')
+for (word, variations) in cmudict.entries():
+    count = min([len([x for x in phonemes if numeral.search(x)]) for phonemes in variations])
+    syllables[word.upper()] = count
+output = open('cmudict.pickle2', 'wb')
+pickle.dump(syllables, output, pickle.HIGHEST_PROTOCOL)
+output.close()
